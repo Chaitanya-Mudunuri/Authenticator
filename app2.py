05de8d928e8171926fafac4236b9d920f6415548
Chaitanya Mudunuri <108880206+Chaitanya-Mudunuri@users.noreply.github.com>
2025-04-26 09:05:28 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -609,6 +609,15 @@ placeholder = st.empty()
 image_file = st.file_uploader(
             "Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
 
+progress_text = "Upload in progress. Please wait..."
+my_bar = st.progress(0, text=progress_text)
+
+for percent_complete in range(100):
+    time.sleep(0.01)
+    my_bar.progress(percent_complete + 1, text=progress_text)
+time.sleep(1)
+my_bar.empty()
+
 if image_file:
 
     frame = Image.open(image_file)
